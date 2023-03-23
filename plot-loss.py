7ad46603391d7163dc7d71ce345fd2017cd3cb17
oeway <oeway007@gmail.com>
2023-03-23 17:51:00 +0100

    Add location augmentation config and plot loss

---
+++ plot-loss.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+metric_path = "/home/wei.ouyang/workspace/stable-diffusion/logs/2022-11-22T20-38-03_hpa-ldm-vq-4-hybrid-location/testtube/version_0/metrics.csv"
+
+# load csv file into pandas DataFrame
+df = pd.read_csv(metric_path)
+
+# filter out empty rows based on val/loss_simple column
+df = df[df['val/loss_simple'].notna()]
+
+# create line plot
+plt.plot( df['val/loss_simple'])
+plt.xlabel('T')
+plt.ylabel('Validation Loss (Simple)')
+plt.title('Validation Loss vs T')
+
+# save figure to a file
+plt.savefig('./tmp.png')
